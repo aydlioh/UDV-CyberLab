768ad3cf20db16c547780f75e34ff143198376e7
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-10-10 19:04:30 +0500

    fix: img auth

--- Frontend/src/pages/Auth/ui/RegistrationPage.tsx
+++ Frontend/src/pages/Auth/ui/RegistrationPage.tsx
@@ -2,9 +2,11 @@ import { RegistrationForm } from '@/widgets/auth';
 
 export const RegistrationPage = () => {
   return (
-    <section className="py-[50px] h-full w-full px-[71px] sm:rounded-[35px] bg-white flex flex-col items-center gap-5">
-      <h1 className="text-2xl">Регистрация</h1>
-      <RegistrationForm />
+    <section className="py-[50px] h-full w-full px-[71px] sm:rounded-[35px] bg-white flex">
+      <div className="z-10">
+        <h1 className="text-2xl">Регистрация</h1>
+        <RegistrationForm />
+      </div>
     </section>
   );
 };

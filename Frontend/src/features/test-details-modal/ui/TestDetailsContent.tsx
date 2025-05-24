99c2b234b9921ee06039e67e8ab3db04fb31a89e
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-05-25 00:30:24 +0500

    feat: test ui expired

--- Frontend/src/features/test-details-modal/ui/TestDetailsContent.tsx
+++ Frontend/src/features/test-details-modal/ui/TestDetailsContent.tsx
@@ -28,12 +28,18 @@ export const TestDetailsContent = () => {
     { userId }
   );
 
+  const isExpired = new Date(data.endDate).getTime() < new Date().getTime();
+
   return (
     <ModalContent>
       <ModalBody>
         <section className="w-full flex flex-col sm:gap-[80px] gap-[15px]">
           <TestDetails {...data} testStatus={testStatus} />
-          <TestDetailsActions {...data} testStatus={testStatus} />
+          <TestDetailsActions
+            {...data}
+            testStatus={testStatus}
+            isExpired={isExpired}
+          />
         </section>
       </ModalBody>
     </ModalContent>
--- Frontend/src/features/test-details-modal/ui/actions/TestDetailsActions.tsx
+++ Frontend/src/features/test-details-modal/ui/actions/TestDetailsActions.tsx
@@ -6,18 +6,30 @@ import { FinishedActions } from './FinishedActions';
 
 type TestDetailsActionsProps = ITestDetails & {
   testStatus: ReturnType<typeof getTestStatus>;
+  isExpired: boolean;
 };
 
 export const TestDetailsActions = ({
   id,
   leftAttempts,
   testStatus: { isOwner, isRunning, isOver },
+  isExpired,
 }: TestDetailsActionsProps) => {
   const hasAttempts = Boolean(leftAttempts && leftAttempts > 0);
 
   if (isOwner) return <OwnerActions testId={id} />;
+  if (isOver)
+    return (
+      <FinishedActions withRepeat={hasAttempts && !isExpired} testId={id} />
+    );
+  if (isExpired) return <TestExpired />;
   if (isRunning) return <ResumeAction testId={id} />;
-  if (isOver) return <FinishedActions withRepeat={hasAttempts} testId={id} />;
 
   return <StartAction testId={id} />;
 };
+
+const TestExpired = () => (
+  <div className="flex justify-center items-center font-bold">
+    Тестирование завершено!
+  </div>
+);
